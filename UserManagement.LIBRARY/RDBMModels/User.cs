809fa8e225336a320d1d0034cc46e282bff677c8
Khai Dang <dangquangkhai@outlook.com>
2018-10-31 13:22:24 +0700

    Commit file User.cs

--- UserManagement.LIBRARY/RDBMModels/User.cs
+++ UserManagement.LIBRARY/RDBMModels/User.cs
@@ -29,5 +29,8 @@ namespace UserManagement.LIBRARY.RDBMModels
 
         [StringLength(50)]
         public string Position { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
     }
 }
