cfe75df3910d9b1e1d98f22468eb959376533538
Khai Dang <dangquangkhai@outlook.com>
2018-11-02 00:23:00 +0700

    Add UserManagementController.cs

--- UserManagement.ADMIN/Controllers/UserManagementController.cs
+++ UserManagement.ADMIN/Controllers/UserManagementController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UserManagement.LIBRARY;
+using UserManagement.LIBRARY.RDBMModels;
+using UserManagement.LIBRARY.RDBMProviders;
 
 namespace UserManagement.ADMIN.Controllers
 {
+
     public class UserManagementController : Controller
     {
+        UserProvider _provider = new UserProvider();
         // GET: UserManagement
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetUser()
+        {
+            User[] AllUser = _provider.getAllUser();
+            return Json(new { success = true, content = AllUser}, JsonRequestBehavior.AllowGet);
         }
     }
 }
