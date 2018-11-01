68852d3487c75542a438013612b119c37d13f340
Khai Dang <dangquangkhai@outlook.com>
2018-11-02 00:22:01 +0700

    commit Filw

---
+++ UserManagement.LIBRARY/RDBMProviders/UserProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.LIBRARY.RDBMModels;
+
+namespace UserManagement.LIBRARY.RDBMProviders
+{
+    public class UserProvider: RDBMBaseProvider
+    {
+        public User[] getAllUser()
+        {
+            return base.db.Users.ToArray();
+        }
+    }
+}
